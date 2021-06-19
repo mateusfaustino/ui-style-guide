6da8fce554c21f743390e6f6920f9597157865d0
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-06-19 10:35:36 -0300

    funcions

--- ui-style-guide/src/Components/functions/index.js
+++ ui-style-guide/src/Components/functions/index.js
@@ -1,3 +1,19 @@
 export const handleDefault = (variable, defaultValue) =>
     variable ? defaultValue : variable;
 
+    function searchAdress(url,parameters, handleError,handleResolve) {
+        const parameters = handleDefault(parameters,{
+            method:'GET',
+            mode:'cors',
+            cache:'default'
+        })
+        
+        fetch(url, parameters)
+        .then(response => response.json()
+        ).then((data) => {
+            return(data)
+        })
+        .catch((error) => {
+            handleError()
+        })
+    }
